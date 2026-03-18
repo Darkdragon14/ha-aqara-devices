cc928957feda1048e452c1ee96ed6d71fa7b9b2f
Darkdragon14 <s.tristan@hotmail.fr>
2026-03-18 18:48:59 +0100

    feat: add options flow handler and update API response handling

--- custom_components/ha_aqara_devices/config_flow.py
+++ custom_components/ha_aqara_devices/config_flow.py
@@ -6,6 +6,7 @@ from homeassistant import config_entries
 from homeassistant.core import callback
 
 from .const import DOMAIN
+from .options_flow import OptionsFlowHandler
 
 DATA_SCHEMA = vol.Schema({
     vol.Required("username"): str,
@@ -25,13 +26,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             )
         return self.async_show_form(step_id="user", data_schema=DATA_SCHEMA, errors=errors)
 
+    @staticmethod
     @callback
-    def async_get_options_flow(self, config_entry):
-        return OptionsFlowHandler(config_entry)
-
-class OptionsFlowHandler(config_entries.OptionsFlow):
-    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
-        self.config_entry = config_entry
-
-    async def async_step_init(self, user_input=None):
-        return self.async_create_entry(title="Options", data={})
+    def async_get_options_flow(config_entry):
+        return OptionsFlowHandler()
